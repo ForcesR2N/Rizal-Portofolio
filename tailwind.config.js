0f5d2338efd7a5e30bc9af1715d35f809bd1849b
ForcesR2N <muhammadzuhrizal07@gmail.com>
2025-05-11 02:17:58 +0700

    Fix and improve structure file

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,14 +7,14 @@ module.exports = {
   ],
   theme: {
     extend: {
+      animation: {
+        'pulse': 'pulse 2s cubic-bezier(0.4, 0, 0.6, 1) infinite',
+      },
       keyframes: {
         pulse: {
           '0%, 100%': { opacity: 1 },
           '50%': { opacity: 0.5 },
         },
-      },
-      animation: {
-        pulse: 'pulse 2s cubic-bezier(0.4, 0, 0.6, 1) infinite',
       },
     },
   },
