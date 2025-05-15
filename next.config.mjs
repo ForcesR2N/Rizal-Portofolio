c766937cc25791c4125995ba0e8e4a790936a5f9
ForcesR2N <muhammadzuhrizal07@gmail.com>
2025-05-15 10:17:40 +0700

    Update deploy.yml dan next.config.njs

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     unoptimized: true
   },
   basePath: process.env.NODE_ENV === 'production' ? '/Rizal-Portofolio' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/Rizal-Portofolio' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/Rizal-Portofolio/' : '',
 };
 
 export default nextConfig;
